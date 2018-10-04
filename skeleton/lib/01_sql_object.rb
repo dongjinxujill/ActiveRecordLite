90de24d913b6743ce378260472afbfbba88bb46f
dongjinxujill <32142414+dongjinxujill@users.noreply.github.com>
2018-10-04 11:17:48 -0700

    first commit

---
+++ skeleton/lib/01_sql_object.rb
@@ -0,0 +1,123 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  
+  def self.columns
+    # ...
+    DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM 
+        #{self.table_name}
+    SQL
+      .first.map {|el| el.to_sym}
+  end
+  
+  def attributes
+    # ...
+    @attributes ||= {}
+    
+  end
+
+  def self.finalize!
+
+    self.columns.each do |col|
+      define_method(col) do
+        attributes[col]
+      end
+      define_method("#{col}=") do |val|
+        attributes[col] = val
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    # ...
+    table_name
+  end
+
+  def self.table_name
+    # ...
+    self.to_s.downcase + 's'
+  end
+
+  def self.all
+    # ...
+    res = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    
+    self.parse_all(res)
+    
+    
+    
+  end
+
+  def self.parse_all(results)
+    # ...
+    x = []
+    results.each do |res|
+      x << self.new(res)
+    end
+    x
+  end
+
+  def self.find(id)
+    # ...
+    
+    self.all.find { |obj| obj.id == id }
+
+  end
+
+  def initialize(params = {})
+    # ...
+    params.each do |k,v|
+      if !self.class.columns.include?(k.to_sym)
+        raise "unknown attribute '#{k}'"
+      else
+        send("#{k}=", v)
+      end
+    end
+  end
+
+
+  def attribute_values
+    # ...
+    self.class.columns.map do |el|
+      send("#{el}=", attributes[el])
+    end
+  end
+
+  def insert
+    # ...
+    col_names = self.class.columns.join(',')
+    question_marks = (["?"] * (self.class.columns).length).join(',')
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+    self.id = DBConnection.last_insert_row_id
+    
+  end
+
+ 
+
+  def save
+    # ...
+    
+    if self.id.nil?
+      self.insert 
+    else
+      self.update
+    end
+  end
+end
---
+++ skeleton/lib/03_associatable.rb
@@ -0,0 +1,88 @@
+require_relative '02_searchable'
+require 'active_support/inflector'
+
+# Phase IIIa
+class AssocOptions
+  attr_accessor(
+    :foreign_key,
+    :class_name,
+    :primary_key
+  )
+
+  def model_class
+    # ...
+    self.class_name.constantize
+  end
+
+  def table_name
+    # ...
+    self.class_name.downcase + 's'
+  end
+end
+
+class BelongsToOptions < AssocOptions
+  def initialize(name, options = {})
+    # ...
+    if options[:foreign_key]
+      @foreign_key = options[:foreign_key]
+    else
+      @foreign_key = (name.to_s + '_id').to_sym 
+    end
+    if options[:class_name]
+      @class_name = options[:class_name]
+    else
+      @class_name = name.to_s.camelcase
+    end
+    if options[:primary_key]
+      @primary_key = options[:primary_key]
+    else
+      @primary_key = :id
+    end
+    
+  end
+end
+
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    # ...
+    if options[:foreign_key]
+      @foreign_key = options[:foreign_key]
+    else
+      @foreign_key = (self_class_name + '_id').downcase.to_sym
+    end
+    if options[:class_name]
+      @class_name = options[:class_name]
+    else
+      @class_name = name.to_s.singularize.camelcase
+    end
+    if options[:primary_key]
+      @primary_key = options[:primary_key]
+    else
+      @primary_key = :id
+    end
+  end
+end
+
+module Associatable
+  # Phase IIIb
+  def belongs_to(name, options = {})
+    # ...
+    options = BelongsToOptions.new(name, options = {})
+    define_method() do 
+      send()
+    end
+  end
+
+  def has_many(name, options = {})
+    # ...
+  end
+
+  def assoc_options
+    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+  end
+end
+
+class SQLObject
+  # Mixin Associatable here...
+  extend Associatable
+end
